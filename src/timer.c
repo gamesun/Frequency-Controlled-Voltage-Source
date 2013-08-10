e6a49b409e380ede47ec1ba5ab779b2c63c316bb
gamesun <gamesunyt@gmail.com>
2013-08-10 21:44:14 +0800

    add TimerInit, GetFreq, ISR functions

--- src/timer.c
+++ src/timer.c
@@ -3,4 +3,28 @@
  Coder : gamesun
 *******************************************************************/
 
+#include "common.h"
 #include "timer.h"
+
+static uint un;
+
+void TimerInit( void )
+{
+
+}
+
+// calc the freq
+ulong GetFreq( void )
+{
+    ulong ulfreq;
+
+
+
+    return ulfreq;
+}
+
+/*------------------------------------------- Timer/Counter1 Capture Event --*/
+ISR( TIMER1_CAPT_vect )
+{
+
+}
