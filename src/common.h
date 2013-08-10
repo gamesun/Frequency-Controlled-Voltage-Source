a9c30d6bb6c5d19b9dba3a2ae01043626d083b83
gamesun <gamesunyt@gmail.com>
2013-08-10 21:38:41 +0800

    recode typedefs

--- src/common.h
+++ src/common.h
@@ -20,17 +20,24 @@ typedef enum {
 }BOOLEAN;
 
 
-typedef unsigned char uchar;
-typedef   signed char schar;
-typedef unsigned int  uint;
-typedef   signed int  sint;
-typedef unsigned long ulong;
-typedef   signed long slong;
-typedef BOOLEAN       bool;
+// Reference to <stdint.h>
+typedef unsigned char           uchar;          // 8-bit unsigned type.  short as uc
+typedef   signed char           schar;          // 8-bit signed type.    short as sc
+typedef unsigned int            uint;           // 16-bit unsigned type. short as un
+typedef   signed int            sint;           // 16-bit signed type.   short as sn
+typedef unsigned long int       ulong;          // 32-bit unsigned type. short as ul
+typedef   signed long int       slong;          // 32-bit signed type.   short as sl
+typedef BOOLEAN                 bool;           //
+
+// note
+// This type is not available when the compiler option -mint8 is in effect.
+typedef unsigned long long int  ulonglong;      // 64-bit unsigned type. short as ull
+typedef   signed long long int  slonglong;      // 64-bit signed type.   short as sll
+
 
 typedef union {
-	uchar	c[4];
-	double	d;
+    uchar   c[4];
+    double  d;
 }D2C;
 
 
