fdc26d34c6beb2cf68b94597c0b59d442ab5099a
gamesun <gamesunyt@gmail.com>
2013-08-12 21:21:46 +0800

    add spi module for tlc5615

---
+++ src/spi.c
@@ -0,0 +1,19 @@
+/*******************************************************************
+ File  : spi.c
+ Coder : gamesun
+*******************************************************************/
+
+#include "common.h"
+#include "spi.h"
+
+void SpiInit( void )
+{
+    
+    
+}
+
+
+void SpiTransmit( uchar* pucData, uchar ucLen )
+{
+    
+}
---
+++ src/spi.h
@@ -0,0 +1,14 @@
+/*******************************************************************
+ File  : spi.h
+ Coder : gamesun
+*******************************************************************/
+
+#pragma once
+
+#ifndef __SPI_H__
+#define __SPI_H__
+
+
+extern void SpiTransmit( uchar* pucData, uchar ucLen );
+
+#endif /* __SPI_H__ */
