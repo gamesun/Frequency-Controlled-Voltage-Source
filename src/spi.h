8e92a62aa132e203693edf89f8a7a70c143968c1
gamesun <gamesunyt@gmail.com>
2013-08-12 22:21:51 +0800

    add declaration

--- src/spi.h
+++ src/spi.h
@@ -8,7 +8,9 @@
 #ifndef __SPI_H__
 #define __SPI_H__
 
-
+extern void SpiInit( void );
+extern void SpiTransmit( uchar* pucData, uchar ucLen );
+extern bool SpiIsBusy( void );
 extern void SpiTransmit( uchar* pucData, uchar ucLen );
 
 #endif /* __SPI_H__ */
