3a848b22d41e1d7f31df239fc70bdfb82ae3d4bf
gamesun <gamesunyt@gmail.com>
2013-08-12 22:17:58 +0800

    send data to tlc5615 by using spi

--- src/dac.c
+++ src/dac.c
@@ -5,11 +5,11 @@
 
 #include "common.h"
 #include "dac.h"
-
+#include "spi.h"
 
 #define DAC_V_MIN           0       // 0.00V
 #define DAC_V_REF           100     // 1.00V
-#define DAC_V_MAX           (DAC_V_REF * 1023 / 512)
+#define DAC_V_MAX           (DAC_V_REF * 1023ul / 512ul)
 
 static void SetDacData( uint unVolIn10bits );
 
@@ -20,7 +20,7 @@ static void SetDacData( uint unVolIn10bits );
 void SetVoltage( uint unVol100times )
 {
     if ( DAC_V_MAX < unVol100times ){
-        unVol100times = DAC_VOL_MAX;
+        unVol100times = DAC_V_MAX;
     }
     
     SetDacData( (uint)( (ulong)unVol100times * 512ul / DAC_V_REF ) );
@@ -28,7 +28,17 @@ void SetVoltage( uint unVol100times )
 
 static void SetDacData( uint unVolIn10bits )
 {
+    uchar ucBuff[2];
     
+    if ( SpiIsBusy() ){
+        
+    } else {
+        unVolIn10bits <<= 2;
+        ucBuff[0] = unVolIn10bits >> 8;
+        ucBuff[1] = unVolIn10bits & 0xff;
+        
+        SpiTransmit( ucBuff, 2 );
+    }
 }
 
 
