ae1910e89dff82815d43ac3a1f4c2c4ef8ab6b34
gamesun <gamesunyt@gmail.com>
2013-08-14 23:29:05 +0800

    add macro MIN(), MAX()

--- src/common.h
+++ src/common.h
@@ -17,6 +17,10 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+
+#define MIN(a, b)       ( (a < b) ? a : b )
+#define MAX(a, b)       ( (a > b) ? a : b )
+
 typedef enum {
     FALSE = 0,
     TRUE  = 1
