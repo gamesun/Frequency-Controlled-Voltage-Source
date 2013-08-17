80e11ab4daaa65faf70a512663ceaf219b2ca504
gamesun <gamesunyt@gmail.com>
2013-08-17 19:23:32 +0800

    add setting.h which defines STAGE_NUM

---
+++ src/setting.h
@@ -0,0 +1,13 @@
+/*******************************************************************
+ File  : setting.h
+ Coder : gamesun
+*******************************************************************/
+
+#pragma once
+
+#ifndef SETTING_H_
+#define SETTING_H_
+
+#define STAGE_NUM           6
+
+#endif /* SETTING_H_ */
