c1d814a2bd75774efdb86032bb1f88184e2c1535
gamesun <gamesunyt@gmail.com>
2013-08-18 16:36:27 +0800

    add GetStage()

--- src/setting.h
+++ src/setting.h
@@ -10,6 +10,15 @@
 #ifndef SETTING_H_
 #define SETTING_H_
 
+#define STAGE_1             0
+#define STAGE_2             1
+#define STAGE_3             2
+#define STAGE_4             3
+#define STAGE_5             4
+#define STAGE_6             5
+
 #define STAGE_NUM           6
+
+extern uchar GetStage( void );
 
 #endif /* SETTING_H_ */
