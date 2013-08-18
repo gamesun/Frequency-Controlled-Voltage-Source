a33b337dd68df482328364166f2af5f97d69fc13
gamesun <gamesunyt@gmail.com>
2013-08-18 11:33:14 +0800

    modify "5000mV" -> "4995mV" in strings

--- src/cmd.c
+++ src/cmd.c
@@ -293,7 +293,7 @@ static void CmdSetVol( PST_CMD pstCmd )
         pgmputs( "bad arguments.\n\n" );
         pgmputs( "usage: setvol i v\n" );
         pgmputs( "  i -> Index of one voltage, from 1 to 6.\n" );
-        pgmputs( "  v -> the Voltage, from 0 to 5000 (mV).\n" );
+        pgmputs( "  v -> the Voltage, from 0 to 4995 (mV).\n" );
     }
 }
 
@@ -312,7 +312,7 @@ static void CmdVolt( PST_CMD pstCmd )
     } else {
         pgmputs( "bad arguments.\n\n" );
         pgmputs( "usage: volt v\n" );
-        pgmputs( "  v -> from 0 to 5000 (mV).\n" );
+        pgmputs( "  v -> from 0 to 4995 (mV).\n" );
     }
 }
 
