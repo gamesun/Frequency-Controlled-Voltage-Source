582452cc90aa46f1230f9093585c2ad709288c40
gamesun <gamesunyt@gmail.com>
2013-08-22 23:03:01 +0900

    modify marcos to match to API

--- src/setting.h
+++ src/setting.h
@@ -7,20 +7,20 @@
 
 #pragma once
 
-#ifndef SETTING_H_
-#define SETTING_H_
+#ifndef __SETTING_H__
+#define __SETTING_H__
 
 #define STAGE_NONE          255
-#define STAGE_1             0
-#define STAGE_2             1
-#define STAGE_3             2
-#define STAGE_4             3
-#define STAGE_5             4
-#define STAGE_6             5
+#define STAGE_1             1
+#define STAGE_2             2
+#define STAGE_3             3
+#define STAGE_4             4
+#define STAGE_5             5
+#define STAGE_6             6
 
 #define STAGE_NUM           6
 
 extern uchar GetStage( void );
 extern void SetStageAndVolt( uchar ucStage );
 
-#endif /* SETTING_H_ */
+#endif /* __SETTING_H__ */
