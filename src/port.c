73c75ce3c45f770ef283bc13ba1ab4d909dc319b
gamesun <gamesunyt@gmail.com>
2013-09-09 22:43:25 +0900

    change the trigger event of EN signal falling edge and rising edge.

--- src/port.c
+++ src/port.c
@@ -51,12 +51,12 @@ static void EnSignalTrigger( void )
 {
     if ( signal_EN == FALLING_EDGE ){
         signal_EN = NONE_EDGE;
+        SetStageAndVolt( STAGE_NONE );
+    } else if ( signal_EN == RISING_EDGE ){
+        signal_EN = NONE_EDGE;
         CounterStart();
         SetStageAndVolt( STAGE_1 );
         SetTimerByMillisecond( GetTTable( 1 ), Time1IsUp );
-    } else if ( signal_EN == RISING_EDGE ){
-        signal_EN = NONE_EDGE;
-        SetStageAndVolt( STAGE_NONE );
     }
 }
 
